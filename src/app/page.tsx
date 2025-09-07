ab182168c4a9e64f3f06924d342880498272c339
Afnan Khalid <afnank19@gmail.com>
2025-09-07 16:41:58 +0500

    feat: add pagination, but needs to be implemented better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { dmSans, instrument } from "../fonts";
 
-export default async function Home() {
+export default async function Home({ searchParams} : any) {
+  const sp = await searchParams;
+  let page = sp.page
+
+  if (page == undefined) {
+    page = 1;
+  }
+  console.log("current page", page, typeof(page))
+
   const cookieStore = await cookies()
 
   const accessToken = cookieStore.get("strava_token");
   console.log("acc", accessToken)
 
-  const response = await fetch("https://www.strava.com/api/v3/athlete/activities?page=1&per_page=5", {
+  const response = await fetch(`https://www.strava.com/api/v3/athlete/activities?page=${page}&per_page=5`, {
     headers: {
       "Authorization": "Bearer " + accessToken?.value
     }
@@ -29,10 +37,12 @@ export default async function Home() {
                   <p className={dmSans.className + " font-bold"}>{run.name}</p>
                   <p>Distance: {(run.distance/1000).toFixed(1)}K</p>
                   <p>Avg Heart Rate: {run.average_heartrate} bpm</p>
+                  <p>Time: {(run.moving_time / 60).toFixed(1)} min</p>
                 </div>
               </Link>
             )
           })}
+          <Link href={`/?page=${parseInt(page)+1}`}>Next Page</Link>
         </div>
       </div>
     </div>
